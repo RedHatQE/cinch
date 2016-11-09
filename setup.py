2a7f8d7d538c5dd49e53d693e2653611b884cc97
Gregory Hellings <ghelling@redhat.com>
2016-11-09 14:07:57 -0500

    Setting version 0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='cinch',
-    version='0.1.6',
+    version='0.2.0',
     description='Cinch continuous integration setup',
     long_description=description,
     url='https://github.com/RedHatQE/cinch',
