d47d888aa830d7a3446cd84f664410302217a698
Gregory Hellings <greg.hellings@gmail.com>
2017-03-20 18:21:52 +0000

    Final lint failure cleanup

--- tests/install.sh
+++ tests/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -ve
 pip install -U pip
-pip install . .[lint] .[test]
+pip install . ".[lint]" ".[test]"
 for image in greghellings/centos_base:7 \
              greghellings/centos_base:6; do
 	docker pull "${image}"
