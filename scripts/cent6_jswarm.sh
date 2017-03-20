08c340a31671a7e2e790ceed4e3c6f0baec34435
Gregory Hellings <greg.hellings@gmail.com>
2017-03-20 17:35:34 +0000

    CentOS 6 container building

--- scripts/cent6_jswarm.sh
+++ scripts/cent6_jswarm.sh
@@ -8,11 +8,11 @@ echo "Building cinch container for version ${CINCH_VERSION}"
 echo "*****************************************************"
 
 echo "Starting container"
-ansible -i /dev/null localhost -m docker_container -a "image=greghellings/centos_base:latest name=jswarm detach=true tty=true command=/bin/bash"
+ansible -i /dev/null localhost -m docker_container -a "image=greghellings/centos_base:6 name=jswarm detach=true tty=true command=/bin/bash"
 echo "Building container with Ansible"
-ansible-playbook -i "${CINCH}/inventory/cent7_jswarm_docker" "${CINCH}/cinch/site.yml" \
+ansible-playbook -i "${CINCH}/inventory/cent6_jswarm_docker" "${CINCH}/cinch/site.yml" \
 	-e jenkins_user_password=some_dummy_value
 echo "Committing container at tag ${CINCH_VERSION}"
 docker commit \
 	--change 'ENTRYPOINT ["/usr/local/bin/dockerize", "-template", "/etc/sysconfig/jenkins_swarm:/etc/sysconfig/jenkins_swarm.templated", "/opt/jswarm.sh"]' \
-	jswarm cinch:cent7-${CINCH_VERSION}
+	jswarm cinch:cent6-${CINCH_VERSION}
