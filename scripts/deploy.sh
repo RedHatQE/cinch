cc6a7533d9c97214ffb5f9bc06457fdf739136fa
Gregory Hellings <greg.hellings@gmail.com>
2017-04-17 15:47:50 -0500

    Review improvements
    Wrapped shell script lines at 80 characters
    Added informative echo to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash -e
 
 CINCH="$(readlink -f "$(dirname "$0")/../")"
-# Extracts the version line from the setup.py script, and trims off the rest of the line to leave
-# only the expected version
-CINCH_VERSION=$(grep "${CINCH}/setup.py" -e 'version=' | sed -e "s/.*version='\(.*\)'.*/\1/")
+# Extracts the version line from the setup.py script, and trims off the rest
+# of the line to leave only the expected version
+CINCH_VERSION=$(grep "${CINCH}/setup.py" -e 'version=' \
+	| sed -e "s/.*version='\(.*\)'.*/\1/")
+echo "Deploying version '${CINCH_VERSION}'"
 
-# Tag the CentOS 7 image as the "latest" version, for people wanting the cutting edge
-docker tag "redhatqecinch/jenkins_slave:cent7-${CINCH_VERSION}" redhatqecinch/jenkins_slave:latest
-# Login to Docker - these environment variables are stored in TravisCI for safe keeping
+# Tag the CentOS 7 image as the "latest" version, for people wanting the
+# cutting edge
+docker tag "redhatqecinch/jenkins_slave:cent7-${CINCH_VERSION}" \
+	redhatqecinch/jenkins_slave:latest
+# Login to Docker - these environment variables are stored in TravisCI for
+# safe keeping
 docker login -p "${DOCKER_PASSWORD}" -u "${DOCKER_USER}"
 # Push the latest builds of these images to Docker Hub
 docker push "redhatqecinch/jenkins_slave:cent6-${CINCH_VERSION}"
