f0d798f350db20da92e11a534a4977503565f3d4
Gregory Hellings <greg.hellings@gmail.com>
2017-04-17 23:29:29 -0500

    Streamling pushing
    Rather than build the jenkins_slave images multiple times
    as docker images - testing, and before_deploy, and during
    deploy, the images built as part of the testing should be
    directly used to upload. Since build jobs time out after
    20 minutes on the free infrastructure, this will help to
    reduce the burden on deployments.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,21 +5,23 @@ CINCH="$(readlink -f "$(dirname "$0")/../")"
 # of the line to leave only the expected version
 CINCH_VERSION=$(grep "${CINCH}/setup.py" -e 'version=' \
 	| sed -e "s/.*version='\(.*\)'.*/\1/")
+DISTRO="${1}"
+LATEST="${2}"
 
-# Build the images before deployment
-"${CINCH}/scripts/centos_jswarm.sh" 6
-"${CINCH}/scripts/centos_jswarm.sh" 7
+echo "Deploying version '${CINCH_VERSION}' to '${DISTRO}'"
 
-echo "Deploying version '${CINCH_VERSION}'"
-
-# Tag the CentOS 7 image as the "latest" version, for people wanting the
-# cutting edge
-docker tag "redhatqecinch/jenkins_slave:cent7-${CINCH_VERSION}" \
-	redhatqecinch/jenkins_slave:latest
 # Login to Docker - these environment variables are stored in TravisCI for
 # safe keeping
 docker login -p "${DOCKER_PASSWORD}" -u "${DOCKER_USER}"
 # Push the latest builds of these images to Docker Hub
-docker push "redhatqecinch/jenkins_slave:cent6-${CINCH_VERSION}"
-docker push "redhatqecinch/jenkins_slave:cent7-${CINCH_VERSION}"
-docker push redhatqecinch/jenkins_slave:latest
+docker push "redhatqecinch/jenkins_slave:${DISTRO}-${CINCH_VERSION}"
+
+# Only one version should be tagged as the latest, so let this be it
+if [ x"${LATEST}" = "xtrue" ]; then
+	echo "Tagging '${DISTRO}' as latest and pushing"
+	# Tag the image as the "latest" version, for people wanting the
+	# cutting edge
+	docker tag "redhatqecinch/jenkins_slave:${DISTRO}-${CINCH_VERSION}" \
+		redhatqecinch/jenkins_slave:latest
+	docker push redhatqecinch/jenkins_slave:latest
+fi
