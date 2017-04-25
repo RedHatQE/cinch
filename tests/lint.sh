62055568ecdd0e898c63d100c88d6344e08a0ec6
Gregory Hellings <greg.hellings@gmail.com>
2017-04-25 18:59:51 -0500

    Fix lint to error on errors
    Fix the lint script so it actually exits on errors
    Fix dangling lint

--- tests/lint.sh
+++ tests/lint.sh
@@ -4,28 +4,23 @@ set -ve
 ###############################################################################
 # ANSIBLE LINT
 ###############################################################################
-find cinch -maxdepth 1 -name '*.yml' \
-	-execdir ansible-playbook \
+find cinch -maxdepth 1 -name '*.yml' -print0 |
+	xargs -0 -L 1 ansible-playbook \
 	             --syntax-check \
-	             -i ../inventory/sample/hosts \
-	             '{}' \;
-find cinch -maxdepth 1 -name '*.yml' \
-	-execdir ansible-lint \
-	             '{}' \;
+	             -i inventory/sample/hosts
+find cinch -maxdepth 1 -name '*.yml' -print0 |
+	xargs -0 -L 1 ansible-lint
 ###############################################################################
 # SHELL LINT
 ###############################################################################
-find . -name '*.sh' -not -name 'jswarm.sh' \
-	-execdir shellcheck \
-	             -e 1090,1091,2093 \
-	             '{}' \;
-find . -name 'jswarm.sh' \
-	-execdir shellcheck \
-	             -e 1090,1091,2093,2086 \
-	             '{}' \;
+find . -name '*.sh' -not -name 'jswarm.sh' -print0 |
+	xargs -0 -L 1 shellcheck \
+	             -e 1090,1091,2093
+find . -name 'jswarm.sh' -print0 |
+	xargs -0 -L 1 shellcheck \
+	             -e 1090,1091,2093,2086
 ###############################################################################
 # PYTHON LINT
 ###############################################################################
-find . -name '*.py' \
-	-execdir flake8 \
-	             '{}' \;
+find . -name '*.py' -print0 |
+	xargs -0 -L 1 flake8
