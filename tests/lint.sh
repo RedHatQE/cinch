6f9cb0f4cdb3ee8390b548c75754a8431869cdde
Gregory Hellings <greg.hellings@gmail.com>
2017-05-03 12:05:26 -0500

    Allow lint.sh to be run from anywhere

--- tests/lint.sh
+++ tests/lint.sh
@@ -1,36 +1,38 @@
 #!/usr/bin/env sh
 set -ve
 
+cinch="$(readlink -f "$(dirname "${0}")/../")"
 ###############################################################################
 # ANSIBLE LINT
 ###############################################################################
-find . \( \( -path './build*' -o -path ./.git \) -prune -o -name '*.yml' \) \
+find "${cinch}" \( \( -path "${cinch}/build" -o -path "${cinch}/.git" \) \
+	-prune -o -name '*.yml' \) \
 	-type f -print0 |
 	xargs -0 -L 1 yamllint \
-	                  -c tests/yamllint.yml
-yamllint -c tests/yamllint.yml cinch/group_vars/*
-find cinch -maxdepth 1 -name '*.yml' -print0 |
+	                  -c "${cinch}/tests/yamllint.yml"
+yamllint -c "${cinch}/tests/yamllint.yml" "${cinch}"/cinch/group_vars/*
+find "${cinch}/cinch" -maxdepth 1 -name '*.yml' -print0 |
 	xargs -0 -L 1 ansible-playbook \
 	             --syntax-check \
-	             -i inventory/sample/hosts
-find cinch -maxdepth 1 -name '*.yml' -print0 |
+	             -i "${cinch}/inventory/sample/hosts"
+find "${cinch}/cinch" -maxdepth 1 -name '*.yml' -print0 |
 	xargs -0 -L 1 ansible-lint \
-	             -R -r tests/ansible_lint_rules/
+	             -R -r "${cinch}/tests/ansible_lint_rules/"
 ###############################################################################
 # SHELL LINT
 ###############################################################################
-find . -name '*.sh' -not -name 'jswarm.sh' -print0 |
+find "${cinch}" -name '*.sh' -not -name 'jswarm.sh' -print0 |
 	xargs -0 -L 1 shellcheck \
 	             -e 1090,1091,2093
 # jswarm.sh specifically includes variables that need to be word-split based
 # on user input. Error 2086 is the Shell warning to quote variables to avoid
 # space splitting. However, two of the variables in this shell script are
 # intended to be split when they get included as variables into the script.
-find . -name 'jswarm.sh' -print0 |
+find "${cinch}" -name 'jswarm.sh' -print0 |
 	xargs -0 -L 1 shellcheck \
 	             -e 1090,1091,2093,2086
 ###############################################################################
 # PYTHON LINT
 ###############################################################################
-find . -name '*.py' -print0 |
+find "${cinch}" -name '*.py' -print0 |
 	xargs -0 -L 1 flake8
