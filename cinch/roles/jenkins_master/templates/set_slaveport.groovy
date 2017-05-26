3d9d313381690c2485a4662ccdf4aad4c06eb61f
David Roble <droble@redhat.com>
2017-05-26 15:11:56 -0400

    added check_mode support to Jenkins configuration groovy scripts
    * Added an optional jenkins_check_mode tag to only check the Jenkins
    configuration

--- cinch/roles/jenkins_master/templates/set_slaveport.groovy
+++ cinch/roles/jenkins_master/templates/set_slaveport.groovy
@@ -1,14 +1,24 @@
 import jenkins.model.Jenkins
 def jenkins = Jenkins.instance
+// Ansible+Jinja makes the boolean value from the template into leading
+// uppercase, which is not valid in Groovy.  We use |to_json to ensure that the
+// boolean value is lowercase.
+def check_mode = {{ ansible_check_mode|to_json }}
 
 def slave_agent_port = jenkins.getSlaveAgentPort()
 int jenkins_slave_agent_port = {{ jenkins_slave_agent_port|int }}
 
+def change_msg = "CHANGED: slave agent port from " + slave_agent_port +
+    " to " + jenkins_slave_agent_port
+
 if (slave_agent_port != jenkins_slave_agent_port) {
-    jenkins.setSlaveAgentPort(jenkins_slave_agent_port)
-    jenkins.save()
-    println "CHANGED: slave agent port from " + slave_agent_port + " to " + \
-        jenkins_slave_agent_port
+    if (check_mode == true) {
+        println change_msg
+    } else {
+        jenkins.setSlaveAgentPort(jenkins_slave_agent_port)
+        jenkins.save()
+        println change_msg
+    }
 } else {
     println "No changes to slave agent port necessary"
 }
