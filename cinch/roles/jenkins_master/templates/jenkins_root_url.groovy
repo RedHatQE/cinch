1a2049946985a9497b9dc04d6797b00cbc4674c9
Gregory Hellings <greg.hellings@gmail.com>
2017-07-19 08:49:33 -0500

    Added Jenkins root URL Groovy script

---
+++ cinch/roles/jenkins_master/templates/jenkins_root_url.groovy
@@ -0,0 +1,17 @@
+import jenkins.model.JenkinsLocationConfiguration;
+import org.apache.commons.lang3.StringUtils;
+
+String newUrl = "{{ _jenkins_url }}";
+boolean changed = false;
+def jlc = JenkinsLocationConfiguration.get()
+def check_mode = {{ ansible_check_mode|to_json }};
+String oldUrl = StringUtils.stripEnd(jlc.getUrl(), "/");
+
+if( !oldUrl.equals(newUrl) ) {
+	jlc.setUrl(newUrl);
+	print "CHANGED: Updated base URL to " + newUrl;
+	changed = true;
+}
+
+if( !check_mode && changed )
+	jlc.save();
