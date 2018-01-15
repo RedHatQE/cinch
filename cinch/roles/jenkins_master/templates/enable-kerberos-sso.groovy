f4266761a1ddb70036204a67ac34eee8a4e23861
Oliver Gondža <ogondza@gmail.com>
2018-01-15 10:23:13 +0100

    Prepare kerberos groovy script for anonymous access configuration

--- cinch/roles/jenkins_master/templates/enable-kerberos-sso.groovy
+++ cinch/roles/jenkins_master/templates/enable-kerberos-sso.groovy
@@ -1,6 +1,10 @@
-def check_mode = {{ ansible_check_mode|to_json }}
+boolean check_mode = {{ ansible_check_mode|to_json }}
+boolean anonymous_access = {{ jenkins_kerberos_anonymous_access|to_json }}
 
 def plugin = com.sonymobile.jenkins.plugins.kerberossso.PluginImpl.getInstance()
+
+// TODO: Since kerberos-sso 1.4 there is no need to use the private field
+// accessor ('@') for any of the fields.
 
 if (!plugin.@enabled) {
   if (!check_mode) plugin.@enabled = true
@@ -8,17 +12,22 @@ if (!plugin.@enabled) {
 }
 
 // Need this to prevent a NPE on startup
-if (!check_mode)  plugin.@password = hudson.util.Secret.fromString("changeme")
+if (!check_mode) plugin.@password = hudson.util.Secret.fromString("changeme")
 println "Setting dummy password"
 
 if (plugin.@allowLocalhost) {
-  if (!check_mode)  plugin.@allowLocalhost = false
+  if (!check_mode) plugin.@allowLocalhost = false
   println "Disabling auto-login from localhost"
 }
 
 if (plugin.@allowUnsecureBasic) {
   if (!check_mode) plugin.@allowUnsecureBasic = false
   println "Disabling unsecured Basic Authentication"
+}
+
+if (plugin.@anonymousAccess != anonymous_access) {
+  if (!check_mode) plugin.@anonymousAccess = anonymous_access;
+  println "${anonymous_access ? "Enabling" : "Disabling"} anonymous access"
 }
 
 if (!check_mode) plugin.save()
