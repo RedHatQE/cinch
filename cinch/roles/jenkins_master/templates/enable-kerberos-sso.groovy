f185aa3fc2d432253a35992b513b028769f9da5e
Oliver Gondža <ogondza@gmail.com>
2018-01-15 09:52:01 +0100

    Add check_mode support for kerberos sso groovy script

--- cinch/roles/jenkins_master/templates/enable-kerberos-sso.groovy
+++ cinch/roles/jenkins_master/templates/enable-kerberos-sso.groovy
@@ -1,21 +1,24 @@
+def check_mode = {{ ansible_check_mode|to_json }}
+
 def plugin = com.sonymobile.jenkins.plugins.kerberossso.PluginImpl.getInstance()
-// Enable SSO
-def f = plugin.getClass().getDeclaredField("enabled");
-f.setAccessible(true);
-println(f.get(plugin))
-f.set(plugin, true);
+
+if (!plugin.@enabled) {
+  if (!check_mode) plugin.@enabled = true
+  println "Enabling Kerberos SSO"
+}
+
 // Need this to prevent a NPE on startup
-def p = plugin.getClass().getDeclaredField("password");
-p.setAccessible(true);
-println(p.get(plugin))
-p.set(plugin, hudson.util.Secret.fromString("changeme"));
-// Disable localhost
-def l = plugin.getClass().getDeclaredField("allowLocalhost");
-l.setAccessible(true);
-l.set(plugin, false);
-// Disable unsecured Basic
-def usb = plugin.getClass().getDeclaredField("allowUnsecureBasic");
-usb.setAccessible(true);
-usb.set(plugin, false);
-// Save
-plugin.save()
+if (!check_mode)  plugin.@password = hudson.util.Secret.fromString("changeme")
+println "Setting dummy password"
+
+if (plugin.@allowLocalhost) {
+  if (!check_mode)  plugin.@allowLocalhost = false
+  println "Disabling auto-login from localhost"
+}
+
+if (plugin.@allowUnsecureBasic) {
+  if (!check_mode) plugin.@allowUnsecureBasic = false
+  println "Disabling unsecured Basic Authentication"
+}
+
+if (!check_mode) plugin.save()
