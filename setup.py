2c6065ac38e8f4576c57f6892c4d87e75d645460
Alexander Braverman Masis <abraverm@users.noreply.github.com>
2019-02-21 08:39:04 +0200

    Release 1.4.0 (#248)
    * Release 1.4.0
    
    * update changelog with version 1.4.0 changes

--- setup.py
+++ setup.py
@@ -7,21 +7,22 @@ with open(path.join(here, 'README.md')) as f:
 
 setup(
     name='cinch',
-    version='1.3.0',
+    version='1.4.0',
     description='Cinch continuous integration setup',
     long_description=description,
     url='https://github.com/RedHatQE/cinch',
     author='RedHatQE',
     license='GPLv3',
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
     ],
     keywords='continuous integration, ci, jenkins',
     packages=find_packages(exclude=('library', 'bin')),
